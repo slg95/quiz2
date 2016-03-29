241a79a244f9ca4d9806a6a83d8452c845f6b598
Spencer Gray <slg94@pitt.edu>
2016-03-28 21:32:26 -0400

    Submit

--- template/quiz2.js
+++ template/quiz2.js
@@ -1,39 +1,84 @@
-(function($){
+(function()
+{
 	// This is where you will write your function for the programming challenge
 	// Do not commit console.log statements
 	// Use ajax to reach the api endpoint
 	// Whether or not you use jQuery, you still have to fix the jQuery errors. Removing jQuery is not fixing the problem.
 
-	$mouseover = $('.mouseover');
-	$click     = $('.clikc');
-	$sub       = $('.submit');
-
-	$mouseover.on('mouseover', function() {
-		$this = $(this);
+	$mouseoverHeight = $('#mouse-over').height(); //keeps the height constant instead of growing
+	
+	$('#mouse-over').mouseover(function()
+	{
 		$(this).html('Scrooge McDuck!');
-		$(this).height($(this).height() + 50);
+		$(this).height($mouseoverHeight + 50);
 	});
-
-	$click.click('click', function() {
-		$this.hmtl('Peace Out!')
-		$(this).fadeout(1500);
+	
+	$('#click').click(function() 
+	{
+		$(this).html('Peace Out!')
+		$(this).fadeOut(1500);
 		return false;
 	});
 
-	$submit.on('submit', function(e) {
+	$('#submit').submit(function(e) {
 		e.preventDefault();
-		if ($(this).find('input[type="text"]').val() !== '') {
-			$(this).find('input').foreach(function() {
-				$(this).fadeout('slow');
+		if($(this).find('input[type="text"]').val() !== '') 
+		{
+			$(this).find('input').each(function() {
+				$(this).fadeOut('slow');
 			});
-			$(this).appendwith('<h2>Congratulations! You've entered some text!</h2>');
+			$(this).append('<h2>Congratulations! You\'ve entered some text!</h2>');
 		}
 	});
 
-	$(document).on(ready, function() {
-		setTimeout({
-			$timeout.fadeIn('slow');
+	$(document).ready(function() 
+	{
+		document.getElementById("change").style.visibility = "hidden";
+		document.getElementById("keep").style.visibility = "hidden";
+		var x = document.cookie;
+		if(x)
+		{
+			var s = x.split(";")
+			document.getElementById("info").innerHTML = "COOKIE: " + s[0];
+		}
+		setTimeout(function()
+		{
+			$("#timeout").fadeOut('slow');
 		}, 1000);
 	});
+	
+	var requested = "";
+	var dataGotten = false;
+	var currentData = "";
+	
+	$("#BUTTON").click(function()
+	{
+		document.getElementById("change").style.visibility = "visible";
+		document.getElementById("keep").style.visibility = "visible";
+		document.getElementById("BUTTON").style.visibility = "hidden";
+		
+		 $.post( "http://www.mattbowytz.com/simple_api.json?data=quizData", function( data ) {
+		  requested = data.data;
+		  changeData();
+		});
+		
+	});
+	
+	$("#change").click(function()
+	{
+		changeData();
+	});
+	
+	$("#keep").click(function()
+	{
+		document.cookie = currentData + ";expires=Thu, 31 Dec 2017 12:00:00 UTC";
+	});
+	
+	function changeData()
+	{
+		rando = Math.floor((Math.random() * requested.length));
+		document.getElementById("info").innerHTML = requested[rando];
+		currentData = requested[rando];
+	}
 
-})(jQuery);
+})();
